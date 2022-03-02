fd723b591f958a8c73dc7745abc9778b346251af
birme <jonas.birme@eyevinn.se>
2022-03-02 21:16:33 +0100

    fix: removed colorized logs as they are problematic in some contexts

--- logging/logger.ts
+++ logging/logger.ts
@@ -26,7 +26,6 @@ winston.addColors(colors);
 
 const format = winston.format.combine(
   winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
-  winston.format.colorize({ all: true }),
   winston.format.printf(
     (info) => `${info.timestamp} ${info.level}: ${info.message}`
   )
