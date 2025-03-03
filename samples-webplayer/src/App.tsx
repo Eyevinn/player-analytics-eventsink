8c5ff2c7e0c093a6e8b5f0cea39bd5063b771248
Ramanjot Kaur <79700214+iamramanjot@users.noreply.github.com>
2025-03-03 13:32:31 +0100

    Sample eventsink webapp

---
+++ samples-webplayer/src/App.tsx
@@ -0,0 +1,22 @@
+// src/App.tsx
+import React, { useState } from "react";
+import "./App.css";
+import Player from "./components/Player";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Eyevinn Event Sink websample</h1>
+      <div className="card">
+        <Player src="https://eyevinnlab-devguide.minio-minio.auto.prod.osaas.io/devguide/VINN/52e124b8-ebe8-4dfe-9b59-8d33abb359ca/index.m3u8" />
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
