27d088f8d62eb4ac693f8a2bf5d9dcaa361da295
Ramanjot Kaur <79700214+iamramanjot@users.noreply.github.com>
2025-03-06 08:12:31 +0100

    Sample Sdk Eventsink webplayer

--- samples-webplayer/src/App.tsx
+++ samples-webplayer/src/App.tsx
@@ -11,9 +11,7 @@ function App() {
       <h1>Eyevinn Event Sink sample</h1>
       <div className="card">
         <Player src="https://eyevinnlab-devguide.minio-minio.auto.prod.osaas.io/devguide/VINN/52e124b8-ebe8-4dfe-9b59-8d33abb359ca/index.m3u8" />
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
       </div>
     </>
   );
